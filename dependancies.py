18f178f8c22d050eff797097cd43ae9bff20d30d
dh4rm4 <dh4rm4@protonmail.com>
2017-01-11 01:01:34 +0100

    add gestion dependance for UNIX

---
+++ dependancies.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+
+from __future__ import unicode_literals
+import os
+import subprocess
+import sys
+
+class col:
+    LOWRED = '\033[94m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# CHECK IF A CMD EXIST IN YOUR SYSTEM
+def cmd_exists(cmd):
+    return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+
+# CHECK IF PIP IS ALREADY INSTALL
+def check_pip():
+    if (cmd_exists('pip') == False):
+        print 'pip        .....................' + col.BOLD + col.FAIL + 'NOPE' + col.ENDC
+        print 'initialise pip installation:'
+        os.system("apt-get -yqq install pip")
+        print 'pip        .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+    else:
+
+        print 'pip        .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+
+# MANAGE THE DEPENDANCIES
+def dependancies():
+    try:
+        if (sys.version_info < (3,0)):
+            import Tkinter
+        else:
+            import tkinter
+        print 'Tkinter    .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+    except ImportError:
+        print 'Tkinter    .....................' + col.BOLD + col.FAIL + 'NOPE' + col.ENDC
+        print 'initialise Tkinter installation:'
+        if (sys.version_info < (3,0)):
+            os.system('apt-get -yqq install python-tk')
+        else:
+            os.system('apt-get -yqq install python3-tk')
+            os.system('apt-get -yqq install python-imaging-tk')
+            print 'Tkinter    .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+
+    try:
+        import ttk
+        print 'ttk        .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+    except ImportError:
+        print 'ttk        .....................' + col.BOLD + col.FAIL + 'NOPE' + col.ENDC
+
+    try:
+        from PIL import ImageTk, Image
+        print 'PIL        .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+    except ImportError:
+        print 'PIL        .....................' + col.BOLD + col.FAIL + 'NOPE' + col.ENDC
+
+    try:
+        import youtube_dl
+        print 'youtube_dl .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+    except ImportError:
+        print 'youtube_dl .....................' + col.BOLD + col.FAIL + 'NOPE' + col.ENDC
+        print 'initialise youtube_dl installation:'
+        os.system('pip -q install youtube_dl')
+        print 'youtube_dl .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+
+
+
+if os.getuid() == 0:
+    print '      ' + col.UNDERLINE + col.LOWRED + '=== W3lcome r00t ===' + col.ENDC
+    print col.YELLOW + '\nCheck dependencies:' + col.ENDC
+    check_pip()
+    dependancies()
+    print '\n\n' + col.FAIL + col.BOLD + '======== INITIALISATION STREAM_DL =======\n\n' + col.ENDC
+    try:
+        import stream_dl
+        print 'stream_dl  .....................' + col.BOLD + col.OKGREEN + 'OK' + col.ENDC
+    except ImportError:
+        print 'stream_dl  .....................' + col.BOLD + col.FAIL + 'NOPE' + col.ENDC
+else:
+    print col.BOLD + 'I cannot run as a mortal.\n' + col.UNDERLINE + col.FAIL + 'Try again as r00t' + col.ENDC
--- stream_dl.py
+++ stream_dl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7.9
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 from Tkinter import *
 from ttk import *
 from PIL import ImageTk, Image
@@ -34,25 +34,26 @@ def get_infos():
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+        meta = ydl.extract_info(url, download=False)
+    print 'duration    : %s' %(meta['duration'])
+    print 'title       : %s' %(meta['title'])
+
 
 # WINDOWS MANAGEMENT
 win = Tk()
 win.resizable(width=False, height=False)
 win.geometry('{}x{}'.format(550, 420))
 
-
 # BACKHROUND IMAGE
 bk_img = ImageTk.PhotoImage(Image.open("bg.jpg"))
 bk_label = Label(image=bk_img)
 bk_label.place(x=-1, y=-1)#, relwidth=1, relheight=1)
-
 
 # COPY URL OF THE VIDEO :
 url_value = StringVar()
 url_value.set("URL de votre video")
 url_input = Entry(win, textvariable=url_value, width=60)
 url_input.place(x=25, y=25, width=500, height=25)
-
 
 # MANAGE THE FORMAT :
 form_choice = ['Download Format :', 'MP4', 'MP3']
@@ -62,7 +63,6 @@ format_choice = Combobox(win, values = form_choice)
 format_choice.current(0)
 format_choice.place(x=75, y=65)
 
-
 # MANAGE THE Playlist :
 choice_pl = ['Download the playlist :', 'YES', 'NOPE']
 pl_var = StringVar(win)
@@ -71,9 +71,7 @@ pl_choice = Combobox(win, values = choice_pl)
 pl_choice.current(0)
 pl_choice.place(x=75, y=95)
 
-
 validation = Button(win, text="Validate", command=get_infos)
 validation.place(x=75, y=125)
-
 
 win.mainloop()
