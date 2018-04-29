e3e5e955977c0a853d4c643a9885625da4dc1ca7
dh4rm4 <dh4rm4@protonmail.com>
2018-04-29 17:18:34 +0200

    working but unfinished

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, send_file
 from app import app
 from app.options import dlOptions
-#from app.streamDlCore import streamDlCore
+from app.streamDlCore import streamDlCore
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,11 +9,11 @@ from app.options import dlOptions
 def main_page():
     form = dlOptions()
     if form.validate_on_submit():
-       cur_session = streamDlCore(form.videoUrl.data,
+        cur_session = streamDlCore(form.videoUrl.data,
                                      form.quality_list.data,
                                      form.format_list.data,
                                      form.dl_playlist.data)
         cur_session.start()
         archive_path = cur_session.get_path()
-        return send_file('boid')
+        return send_file(archive_path)
     return render_template('main_page.html', title='Stream DL', form=form)
---
+++ src/app/streamDlCore.py
@@ -0,0 +1,103 @@
+from __future__ import unicode_literals
+import youtube_dl
+import random, string, os, shutil
+
+class myLogger(object):
+    """
+    youtube-dl logger
+    """
+    
+    def debug(self, msg):
+        pass
+    
+    def warning(self, msg):
+        pass
+    
+    def error(self, msg):
+        print(msg)
+
+        
+class streamDlCore(object):
+    """
+    Class calling youtube-dl package
+    -> download video
+    -> convert it
+    -> create archive with all converted files
+    """
+
+    def __init__(self, url, quality, vFormat, dl_playlist):
+        self.url = url
+        self.postProcessor = self.get_postProcessor(quality, vFormat)
+        self.vFormat= self.get_vFormat(vFormat, quality)
+        self.dl_playlist = self.need_dl_playlist(dl_playlist)
+        self.dlDir = self.set_dl_dir()
+        
+    def get_postProcessor(self, quality, vFormat):
+        """
+        return dict with options for postDownload process
+        """
+        if quality == 'min':
+            q_kbps = '128'
+        else:
+            q_kbps = '320'
+        if vFormat == 'mp3':
+            return [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': q_kbps,
+            }]
+        return []
+
+    def get_vFormat(self, vformat, quality):
+        if vformat == 'mp3':
+            if quality == 'max':
+                return 'bestaudio'
+            return 'worstaudio'
+        else:
+            if quality == 'max':
+                return 'bestvideo'
+            return 'worstvideo'            
+
+    def need_dl_playlist(self, dl_playlist):
+        """
+        Return boolean for the option: noplaylist
+        """
+        if dl_playlist == 'yes':
+            return False
+        return True
+
+    def get_rand_str(self, a, b):
+        return ''.join([random.choice(string.ascii_lowercase)
+                        for _ in range(random.randint(a, b))])
+    
+    def set_dl_dir(self):
+        """
+        create a tmp directory with random name 
+        and return its name
+        """
+        dirName = self.get_rand_str(8, 8)
+        dirPath = 'dl/' + dirName + '/'
+        if not os.path.exists(dirPath):
+            os.makedirs(dirPath)
+#            os.chmod(dirPath, 0o666)
+        return dirPath
+        
+    def start(self):
+        options = {
+            'format': self.vFormat,
+            'postprocessors': self.postProcessor,
+            'logger': myLogger(),
+            'noplaylist': self.dl_playlist,
+            'ignoreerrors': False,            
+            'outtmpl': self.dlDir + '%(title)s.%(ext)s',
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([self.url])
+
+        archive_name = self.get_rand_str(4, 6)
+        self.archive_path = self.dlDir + archive_name
+        shutil.make_archive(self.archive_path, 'zip', self.dlDir)
+
+    def get_path(self):
+        return ('../' + self.archive_path + '.zip')
+        
