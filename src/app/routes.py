56a7701003d2c4aa48d85fa7c4447f6ff17ea676
dh4rm4 <dh4rm4@protonmail.com>
2018-05-04 18:27:42 +0200

    add 404/500 error + first step dl page

--- src/app/routes.py
+++ src/app/routes.py
@@ -15,5 +15,22 @@ def main_page():
                                    form.dl_playlist.data)
         cur_session.start()
         archive_path = cur_session.get_path()
-        return send_file(archive_path)
+        return render_template('dl_page.html',
+                               title = 'Download Page',
+                               archive_path = archive_path)
     return render_template('main_page.html', title='Stream DL', form=form)
+
+
+@app.route('/<archive_path>')
+def download_page(archive_path):
+    return send_file(archive_path)
+
+
+@app.errorhandler(404)
+def handle_bad_request(error):
+        return render_template('404.html')
+
+@app.errorhandler(500)
+def handle_bad_request(error):
+        return render_template('500.html')
+
